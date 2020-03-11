6114be4939e3875ddfe579c665ef3dd1127cd842
danggai <donggi9313@naver.com>
2020-03-11 09:32:22 +0900

    TwoLineListView Preview uploaded / CustomAdapter Example created.

---
+++ CustomAdapterExample/app/src/main/java/com/example/customadapterexample/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.customadapterexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    var 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
