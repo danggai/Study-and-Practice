3c622c4de4fb0200b222e1a7f9650a6da18aeb81
danggai <donggi9313@naver.com>
2020-03-16 14:19:53 +0900

    SeekBar Example Done.

---
+++ SeekBarExample/README.md
@@ -0,0 +1,11 @@
+﻿# SeekBar Example
+
+ProgressBar와 비슷해 보이는 SeekBar 실습해보기.
+
+# 주요 학습 내용
+
+1. SeekBar의 값이 변경 될 때, 변경되는 값을 꺼내 쓰는 방법.
+
+# Preview
+
+![](https://github.com/danggai/Kotlin-Android-Examples/blob/master/SeekBarExample/preview.gif?raw=true오후 2:19 2020-03-16)
--- SeekBarExample/app/src/main/java/com/example/seekbarexample/MainActivity.kt
+++ SeekBarExample/app/src/main/java/com/example/seekbarexample/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.seekbarexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var listener = SeekListener()
+        seekBar1.setOnSeekBarChangeListener(listener)
+        seekBar2.setOnSeekBarChangeListener(listener)
+        seekBar3.setOnSeekBarChangeListener(listener)
+    }
+
+    inner class SeekListener: SeekBar.OnSeekBarChangeListener {
+        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            when(seekBar) {
+                seekBar1 -> txtView1.text = "Seekbar1 : $progress"
+                seekBar2 -> txtView2.text = "Seekbar2 : $progress"
+                seekBar3 -> txtView3.text = "Seekbar3 : $progress"
+                else -> null
+            }
+        }
+        override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+        override fun onStopTrackingTouch(seekBar: SeekBar?) {}
     }
 }
