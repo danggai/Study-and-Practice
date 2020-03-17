54582832d276a447c058acfc3275fa2dfbb2fa9f
danggai <donggi9313@naver.com>
2020-03-17 17:35:59 +0900

    run activity done.

---
+++ BasicActivityExample/app/src/main/java/com/example/basicactivityexample/SecondActivity.kt
@@ -0,0 +1,17 @@
+package com.example.basicactivityexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        btn1.setOnClickListener {
+            finish()
+        }
+    }
+}
