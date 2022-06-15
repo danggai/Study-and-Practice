24d26facb060fced41cb9a6826165946185290e2
danggai <donggi9313@naver.com>
2022-06-15 13:34:48 +0900

    Jetpack Compose Basics Sample  - step 9-14

--- JetpackComposeBasics/app/src/main/java/danggai/app/jetpackcomposebasics/MainActivity.kt
+++ JetpackComposeBasics/app/src/main/java/danggai/app/jetpackcomposebasics/MainActivity.kt
@@ -1,13 +1,25 @@
 package danggai.app.jetpackcomposebasics
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import danggai.app.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
@@ -23,11 +35,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "DefaultPreviewDark"
+)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicsTheme {
-        Greeting("Android")
+        Greetings()
     }
 }
 
@@ -45,10 +63,10 @@ fun MyApp() {
 }
 
 @Composable
-private fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) {
+            Greeting(name = it)
         }
     }
 }
@@ -61,23 +79,37 @@ private fun Greeting(name: String) {
             .fillMaxWidth()
             .padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        var expended by remember { mutableStateOf(false) }
-        val extraPadding = if (!expended) 0.dp else 48.dp
+        var expanded by remember { mutableStateOf(false) }
 
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(modifier = Modifier.padding(24.dp).padding(12.dp).animateContentSize(
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )) {
             Column(
                 modifier = Modifier
                     .weight(1f)
-                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.Bold)
+                )
+
+                if (expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4),
+                    )
+                }
             }
 
-            OutlinedButton(
-                onClick = { expended = !expended }
-            ) {
-                Text(text = if (!expended) "Show more" else "Show less")
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon (
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) stringResource(R.string.show_less) else stringResource(R.string.show_more)
+                )
             }
         }
     }
