9899382bf1cc4163fe66023939c6e77f8c4d307a
danggai <donggi9313@naver.com>
2022-08-10 08:33:57 +0900

    - ComposeThemingCodelab chapter 6-9 done

--- ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ ThemingCodelab/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -41,6 +42,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.theming.R
@@ -108,6 +110,7 @@ fun Header(
     ) {
         Text(
             text = text,
+            style = MaterialTheme.typography.subtitle2,
             modifier = modifier
                 .fillMaxWidth()
                 .background(Color.LightGray)
@@ -165,11 +168,16 @@ private fun PostMetadata(
         append(divider)
         append(stringResource(R.string.read_time, post.metadata.readTimeMinutes))
         append(divider)
+        val tagStyle = MaterialTheme.typography.overline.toSpanStyle().copy(
+            background = MaterialTheme.colors.primary.copy(alpha = 0.1f)
+        )
         post.tags.forEachIndexed { index, tag ->
             if (index != 0) {
                 append(tagDivider)
             }
-            append(" ${tag.uppercase(Locale.getDefault())} ")
+            withStyle(tagStyle) {
+                append(" ${tag.uppercase()} ")
+            }
         }
     }
     CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
@@ -193,6 +201,7 @@ fun PostItem(
         icon = {
             Image(
                 painter = painterResource(post.imageThumbId),
+                modifier = Modifier.clip(shape = MaterialTheme.shapes.small),
                 contentDescription = null
             )
         },
@@ -205,14 +214,14 @@ fun PostItem(
     )
 }
 
-//@Preview("Post Item")
-//@Composable
-//private fun PostItemPreview() {
-//    val post = remember { PostRepo.getFeaturedPost() }
-//    Surface {
-//        PostItem(post = post)
-//    }
-//}
+@Preview("Post Item")
+@Composable
+private fun PostItemPreview() {
+    val post = remember { PostRepo.getFeaturedPost() }
+    Surface {
+        PostItem(post = post)
+    }
+}
 
 @Preview("Featured Post")
 @Composable
